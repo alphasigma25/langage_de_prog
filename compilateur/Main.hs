da554ef4617a6ce57a4b44fe09ce5902b1736e24
alphasigma25 <61196412+alphasigma25@users.noreply.github.com>
2022-12-06 23:03:59 +0100

    rajout projet compilateur

---
+++ compilateur/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Compil (compiler)
+
+main :: IO ()
+main = print compiler
---
+++ lib/Compil/Compil.hs
@@ -0,0 +1,5 @@
+module Compil where
+-- import Data.Int (Int16)
+
+compiler :: String
+compiler = "hello world"
